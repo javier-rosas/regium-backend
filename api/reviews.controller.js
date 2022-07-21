7d2add0bd48ed797f3e138d7318e4d4aaf15687c
javier-rosas <jr2dj@virginia.edu>
2022-07-20 20:14:27 -0400

    first commit

---
+++ api/reviews.controller.js
@@ -0,0 +1,102 @@
+import ReviewsDAO from "../dao/reviewsDAO.js";
+
+export default class ReviewsController {
+
+    static async apiPostReview(req, res, next) {
+        // TODO:
+
+        try {
+            const movieId = req.body.movie_id 
+            const review = req.body.review 
+            const userInfo = {
+                name : req.body.name,
+                _id : req.body.user_id 
+            }
+
+            const date = new Date()
+
+            const reviewResponse = await ReviewsDAO.addReview(
+                movieId, 
+                userInfo, 
+                review, 
+                date
+            )
+
+            let {error} = reviewResponse 
+            console.log(error)
+
+            if (error) {
+                res.status(500).json( { error : "Unable to post review" } )
+            } else {
+                res.json( { status : "success" } )
+            }
+
+        } catch(e) {
+            res.status(500).json( { error : e.message } )
+        }
+
+    }
+
+    static async apiUpdateReview(req, res, next) {
+        // TODO:
+
+        try {
+            const reviewId = req.body._id
+            const review = req.body.review 
+            const userInfo = {
+                name : req.body.name,
+                _id : req.body.user_id 
+            }
+
+            const date = new Date()
+
+            const reviewResponse = await ReviewsDAO.updateReview(
+                userInfo,
+                reviewId,
+                review, 
+                date
+            )
+
+            let {error} = reviewResponse 
+            console.log(error)
+
+            if (error) {
+                res.status(500).json( { error : "Unable to post review" } )
+            } else {
+                res.json( { status : "success" } )
+            }
+
+        } catch(e) {
+            res.status(500).json( { error : e.message } )
+        }
+
+
+    }
+
+    static async apiDeleteReview(req, res, next) {
+        // TODO:
+
+        try {
+            const reviewId = req.body._id
+            const userId = req.body.user_id 
+
+            const reviewResponse = await ReviewsDAO.deleteReview(
+                reviewId,
+                userId
+            )
+
+            let {error} = reviewResponse 
+            console.log(error)
+
+            if (error) {
+                res.status(500).json( { error : "Unable to post review" } )
+            } else {
+                res.json( { status : "success" } )
+            }
+
+        } catch(e) {
+            res.status(500).json( { error : e.message } )
+        }
+    }
+
+}
---
+++ dao/reviewsDAO.js
@@ -0,0 +1,84 @@
+import mongodb from "mongodb"
+const objectId = mongodb.ObjectId
+
+let reviews;
+
+export default class ReviewsDAO {
+
+    static async injectDB(conn) {
+
+        if (reviews) {
+            return
+        }
+        try {
+            reviews = await conn.db(process.env.MOVIEREVIEWS_NS).collection("reviews")
+
+        } catch(e) {
+            console.log(`Unable to establish a connection handle in reviewsDA: ${e}`)
+        }
+    }
+
+    static async addReview(movieId, user, review, date) {
+        try {
+            const reviewDoc = {
+                name : user.name, 
+                user_id : user._id, 
+                date : date, 
+                review : review, 
+                movie_id : objectId(movieId)
+            }
+            let res = await reviews.insertOne( reviewDoc )
+            return res
+        } catch(e) {
+            console.error(`Unable to post review: ${e}` )
+            return { error : e }
+        }
+    }
+
+    static async updateReview(userInfo, reviewId, review, date) {
+
+        try {
+            const reviewDoc = {
+                name : userInfo.name,
+                user_id : userInfo._id,
+                review : review,
+                date : date
+            }
+            
+            let res = await reviews.updateOne( { _id : new objectId(reviewId) }, { $set: reviewDoc } )
+            let modifiedCount = res.modifiedCount
+            if (!modifiedCount){
+                console.error(`A change was not made` )
+                return
+            }
+            return res
+        } catch(e) {
+            console.error(`Unable to post review: ${e}` )
+            return { error : e }
+        }
+
+    }
+
+    static async deleteReview(reviewId, userId) {
+
+        try {
+            
+            let res = await reviews.deleteOne( { _id : new objectId(reviewId) } )
+
+            let deletedCount = res.deletedCount
+            if (!deletedCount){
+                console.error(`No document deleted` )
+                return
+            }
+            return res
+        } catch(e) {
+            console.error(`Unable to post review: ${e}` )
+            return { error : e }
+        }
+
+    }
+
+
+
+}
+
